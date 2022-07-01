0efae4a57e951dfa374cc48405ce793bb4a1303f
ayuboketch <ayuboketch84@gmail.com>
2022-07-01 02:30:49 -0700

    Programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#!/bin/bash
+#include <stdio.c>
+int main(void) 
+{ put("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+#include <stdio.c>
+int main(void)
+{ int i;
+	long int I;
+	long long int II;
+	float d;
+	char c;
+
+	prinft("Size of a char: %lu byte(s)\n"
+			sizeof(c));
+	prinft("Size of a int: %lu byte(s)\n"
+                        sizeof(i));
+	prinft("Size of a long int: %lu byte(s)\n"
+                        sizeof(I));
+	prinft("Size of a long long int: %lu byte(s)\n"
+                        sizeof(II));
+	prinft("Size of a float: %lu byte(s)\n"
+                        sizeof(d));
+	return(0);
+}
