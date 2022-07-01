029a6962209bb7053710285cc1f106e7a7a403df
ayuboketch <ayuboketch84@gmail.com>
2022-07-01 02:38:50 -0700

    Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,25 @@
 #!/bin/bash
 #include <stdio.h>
+/**
+ * main - entry point
+ * return always 0 (success)
+ */
 int main(void)
-{ int i;
+{ 	int i;
 	long int I;
 	long long int II;
 	float d;
 	char c;
 
-	prinft("Size of a char: %lu byte(s)\n"
+	printf("Size of a char: %lu byte(s)\n"
 			sizeof(c));
-	prinft("Size of a int: %lu byte(s)\n"
+	printf("Size of a int: %lu byte(s)\n"
                         sizeof(i));
-	prinft("Size of a long int: %lu byte(s)\n"
+	printf("Size of a long int: %lu byte(s)\n"
                         sizeof(I));
-	prinft("Size of a long long int: %lu byte(s)\n"
+	printf("Size of a long long int: %lu byte(s)\n"
                         sizeof(II));
-	prinft("Size of a float: %lu byte(s)\n"
+	printf("Size of a float: %lu byte(s)\n"
                         sizeof(d));
 	return(0);
 }
