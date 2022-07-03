40365c908aa881632a41e61e35f8041804856fd1
ayuboketch <ayuboketch84@gmail.com>
2022-07-03 07:22:23 -0700

    First commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,2 +1,27 @@
 #!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'positive or not positive'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+        int last;
+        char str[] - "last digit of";
 
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (last > 5)
+		printf("%s %d is greater than 5\n", str, n, last);
+	else if (last == 0)
+		printf("%s %d is %d and is 0\n", str, n, last);
+	else if (last < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, last);
+	return (0);
+}
