4d4a386b74110a48571eb65c67d7fcb5342079ec
ayuboketch <ayuboketch84@gmail.com>
2022-07-03 07:32:22 -0700

    First commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,21 +10,21 @@
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n")
-	}
-	if (n == 0)
-	{
-		printf("%d is zero\n")
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n")
-	}
-	return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+        {
+                printf("%d is positive\n")
+        }
+        if (n == 0)
+        {
+                printf("%d is zero\n")
+        }
+        if (n < 0)
+        {
+                printf("%d is negative\n")
+        }
+        return (0);
 }
