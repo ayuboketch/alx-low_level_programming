ce58cc7c9d1595e8c4a1b7bab99e3eddea75f2ab
ayuboketch <ayuboketch84@gmail.com>
2022-07-04 12:51:27 -0700

    first commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-int n;
+	int num;
 
-for (n = 0; n < 10; n++)
-{
-printf("%d", n);
-}
-printf("\n");
-return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+
 }
