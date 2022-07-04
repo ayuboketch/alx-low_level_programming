ff045c0c742698ae548263faf417f2909ff15abf
ayuboketch <ayuboketch84@gmail.com>
2022-07-04 13:04:45 -0700

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,21 +5,21 @@
  * return always 0 (success)
  */
 int main(void)
-{ 	int i;
-	long int I;
-	long long int II;
-	float d;
-	char c;
+{ 	int a;
+	long int b;
+	long long int c;
+	float f;
+	char d;
 
 	printf("Size of a char: %lu byte(s)\n",
-			sizeof(c));
+			(unsigned long)sizeof(d));
 	printf("Size of a int: %lu byte(s)\n",
-                        sizeof(i));
+                        (unsigned long)sizeof(a));
 	printf("Size of a long unsigned int: %lu byte(s)\n",
-                        sizeof(I));
+                        (unsigned long)sizeof(b));
 	printf("Size of a long long unsigned int: %lu byte(s)\n",
-                        sizeof(II));
+                        (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n",
-                        sizeof(d));
+                        (unsigned long)sizeof(f));
 	return (0);
 }
