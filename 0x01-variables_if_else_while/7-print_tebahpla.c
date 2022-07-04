ba144253395f901ce45c8e0a7aa349f57b178c55
ayuboketch <ayuboketch84@gmail.com>
2022-07-04 12:49:28 -0700

    first commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	int num;
+	char letter;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 
 	putchar('\n');
 
