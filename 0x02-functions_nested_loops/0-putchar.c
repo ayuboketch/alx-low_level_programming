09c8c4c59354523e993fc980ae66dfee4b713f4d
ayuboketch <ayuboketch84@gmail.com>
2022-07-06 01:16:39 -0700

    First commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,20 +5,12 @@
  * print the character after _putchar(0) = success
  * Return: Always 0
  */
-int _putchar(void)
+int _putchar(void);
 {
 	return (0);
 }
-int main(void)
+int main(void);
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar("_putchar\n");
 	return (0);
 }
