70e139e8ce0dc6ec182a6044cf7f38282f20f14f
ayuboketch <ayuboketch84@gmail.com>
2022-07-07 01:46:44 -0700

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Main checks for positive or negative results
+ * always returns (0) = Success
+ */
+int main(void)
+{
+	int i;
+	i = 0
+		positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is the debugging file which explains all the procedures taken to debug the code using debuggers such as rubber duck
